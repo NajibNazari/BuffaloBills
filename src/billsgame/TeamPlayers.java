7e949fdcc8827cd6a7a4c883cea4ae9dfd234349
nazar <nazar@DESKTOP-MOV89AK>
2021-11-25 14:26:03 -0500

    See new changes...

---
+++ src/billsgame/TeamPlayers.java
@@ -0,0 +1,19 @@
+package billsgame;
+
+public class TeamPlayers {
+
+	public static void main(String[] args) {
+		getInfo("Buffalo Bills", getTotal);
+		
+	}
+	String name = "Buffalo Bills";
+	static int getTotal = 12;
+	
+	public static void getInfo(String name, int getTotal) {
+		
+		
+		System.out.println(name);
+		System.out.println(getTotal);
+		System.out.println("push this code to gitHub");
+	}
+}
